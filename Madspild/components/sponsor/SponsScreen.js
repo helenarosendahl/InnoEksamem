8a9e2a33e7dd99fca3f2bd27f958d7744010b318
Taoemil <taoemil@hotmail.com>
2023-11-25 18:43:17 +0100

    sponsor oplysninger

--- Madspild/components/sponsor/SponsScreen.js
+++ Madspild/components/sponsor/SponsScreen.js
@@ -9,10 +9,10 @@ const SponsScreen = ({ navigation }) => {
     const auth = getAuth();
 
     const sponsors = [
-        { id: 1, name: "Joe & the Juice", logoUrl: "path/to/logo1.png", description: "We care about leaving the Planet in a better state than we found it in therefore reducing our impact on the environment. Not only in our stores, but making it easier for our customers to recycle our products through our Recycling Loop. We'll limit the materials used in our receptacles making recycling simple efficient, whilst also moving to RPET, Bagasse, & Recycled Paper. We'll stay committed to our 2% Food Waste Score by continuing to make everything fresh to order." },
-        { id: 2, name: "Sponsor 2", logoUrl: "path/to/logo2.png", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." },
-        { id: 3, name: "Sponsor 3", logoUrl: "path/to/logo3.png", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." },
-        { id: 4, name: "Sponsor 4", logoUrl: "path/to/logo4.png", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." },
+        { id: 1, name: "Joe & the Juice", logoUrl: require('../../assets/joeLogo.png'), description: "Joe & the Juice cares about leaving the Planet in a better state than we found it in therefore reducing our impact on the environment. Not only in our stores, but making it easier for our customers to recycle our products through our Recycling Loop. We'll stay committed to our 2% Food Waste Score by continuing to make everything fresh to order." },
+        { id: 2, name: "COFOCO", logoUrl: require('../../assets/cofoco.png'), description: "At Copenhagen Food Collective, we are aware that with 16 restaurants and more than 700,000 guests annually, we have an impact on our surroundings - postive and negative. That's why we work hard to reduce our negative impact - and increase our positive contribution - with the ambition to practise social, economic and environmental responsibility in everything we do. " },
+        { id: 3, name: "ALDI Danmark", logoUrl: require('../../assets/aldi.png'), description: "Vi er stolte af vores indsats på madspilds-området. I 2020 donerede 60% af vores butikker overskudsmad til lokale foreninger, og alle butikker sælger lykkeposer med frugt og grønt til stærkt nedsatte priser ... På bare fire måneder i slutningen af 2020 har vi og kunderne reddet 118 ton mad. Vi undgår et stort ressourcespild, og vores kunder sparer penge og får økonomisk råderum til andre vigtige ting i hverdagen." },
+        { id: 4, name: "Sponsor 4", logoUrl: require('../../assets/joeLogo.png'), description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." },
 
       ];
   
@@ -39,10 +39,10 @@ const SponsScreen = ({ navigation }) => {
       <Text style={styles.headerTexttext}> Vores sponsorer, som gør det muligt at drive denne applikation. Se hvilke produkter du kan købe, med dine optjente point! </Text>
         {sponsors.map((sponsor) => (
           <View key={sponsor.id} style={styles.sponsorBox}>
-            <Image source={{ uri: sponsor.logoUrl }} style={styles.logo} />
+        <Image source={sponsor.logoUrl} style={styles.logo} />
             <Text style={styles.sponsorHeadText}>{sponsor.name}</Text>
             <Text style={styles.text}>{sponsor.description}</Text>
-            <Button title={`Se produkter fra ${sponsor.name}`} onPress={() => navigation.navigate('ProductScreen')} />
+            <Button title={`Se tilbuddene fra ${sponsor.name}`} onPress={() => navigation.navigate('ProductScreen')} />
           </View>
         ))}
       </ScrollView>
@@ -61,13 +61,13 @@ const styles = StyleSheet.create({
   sponsorBox: {
     padding: 20,
     marginVertical: 20, // Reduced vertical margin for less spacing
-    backgroundColor: '#ffc0cb', // Pink background color
+    backgroundColor: '#fffff0', // Pink background color
     borderRadius: 10,
     borderWidth: 2, // Adjust as needed
-    borderColor: 'pink', // Pink border color
+    borderColor: 'black', // Pink border color
   },
   logo: {
-    width: 100,
+    width: 300,
     height: 100,
     resizeMode: 'contain',
   },

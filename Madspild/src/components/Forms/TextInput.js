0a335469353b7909570f950deebd6c3a9d6e4dc7
Taoemil <taoemil@hotmail.com>
2023-11-28 16:27:18 +0100

    komponenter integration

--- Madspild/src/components/Forms/TextInput.js
+++
@@ -1,8 +0,0 @@
-// Components/Forms/TextInput.js
-import React from 'react';
-import { TextInput } from 'react-native';
-import { globalStyles } from '../../styles/GlobalStyles';
-
-export const CustomTextInput = (props) => (
-  <TextInput style={globalStyles.input} {...props} />
-);

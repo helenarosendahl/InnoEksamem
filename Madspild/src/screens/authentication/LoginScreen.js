58efb2605a1d6c598d216f10eba1d4cd708125c3
sgnielsen <sgnielsen11@gmail.com>
2023-12-11 22:03:21 +0100

    kommentering login & signup

--- Madspild/src/screens/authentication/LoginScreen.js
+++ Madspild/src/screens/authentication/LoginScreen.js
@@ -1,7 +1,14 @@
+// Importerer nødvendige React Native komponenter
 import React, { useState } from 'react';
 import { View, Alert, StyleSheet, Image, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+
+// Importerer Firebase Authentication funktioner
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+// Importerer Firebase-konfiguration fra firebaseConfig-fil
 import firebaseApp from '../../../firebaseConfig';
+
+// Importerer brugerdefinerede komponenter og stilarter
 import { CustomTextInput } from '../../components/Forms/TextInput';
 import { PrimaryButton } from '../../components/Buttons/PrimaryButton';
 import { SecondaryButton } from '../../components/Buttons/SecondaryButton';
@@ -9,22 +16,29 @@ import { globalStyles } from '../../styles/GlobalStyles';
 import AppLogo from '../../components/Logo/AppLogo';
 
 
+// Funktionen for login
 const LoginScreen = ({ navigation }) => {
+  // Deklarerer variablerne email og adgangskode
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  // Henter Firebase Authentication 
   const auth = getAuth(firebaseApp);
 
+   // Funktion til håndtering af login-processen
   const handleLogin = () => {
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
+        // Viser meddelsesen 'success' hvis login lykkedes 
         Alert.alert("Success", "Logged in successfully!");
       })
       .catch((error) => {
+        // Viser fejlmeddelelsen 'error hvis login fejler
         Alert.alert("Error", error.message);
       });
   };
 
+  // Returnerer viewet for login-skærmen
   return (
     <KeyboardAvoidingView 
       style={globalStyles.container}
@@ -33,6 +47,7 @@ const LoginScreen = ({ navigation }) => {
       <ScrollView>
         <View style={globalStyles.innerContainer}>
           <AppLogo source={require('../../assets/logos/appLogo.png')} />
+          {/* Input-felter for email og adgangskode */}
           <CustomTextInput
             placeholder="Email"
             value={email}
@@ -44,7 +59,9 @@ const LoginScreen = ({ navigation }) => {
             onChangeText={setPassword}
             secureTextEntry
           />
+          {/* Knap til at udføre login-handling */}
           <PrimaryButton title="Log ind" onPress={handleLogin} />
+          {/* Knap til at udføre signup-handling */}
           <SecondaryButton 
             title="Ikke allerede bruger? Tilmeld dig her!" 
             onPress={() => navigation.navigate('SignUpScreen')}
@@ -55,5 +72,5 @@ const LoginScreen = ({ navigation }) => {
   );
 };
 
-
+// Eksporterer LoginScreen-komponenten som standard
 export default LoginScreen;
--- Madspild/src/screens/authentication/SignUpScreen.js
+++ Madspild/src/screens/authentication/SignUpScreen.js
@@ -1,29 +1,43 @@
+// Importerer nødvendige React Native komponenter
 import React, { useState } from 'react';
 import { View, Alert, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+
+// Importerer Firebase Authentication funktioner
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+// Importerer Firebase-konfiguration fra firebaseConfig-fil
 import firebaseApp from '../../../firebaseConfig';
+
+// Importerer brugerdefinerede komponenter og stilarter
 import { CustomTextInput } from '../../components/Forms/TextInput';
 import { PrimaryButton } from '../../components/Buttons/PrimaryButton';
 import { globalStyles } from '../../styles/GlobalStyles';
 import AppLogo from '../../components/Logo/AppLogo';
 
+// Funktionen for signup
 const SignUpScreen = ({ navigation }) => {
+  // Deklarerer variablerne email og adgangskode
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  // Henter Firebase Authentication 
   const auth = getAuth(firebaseApp);
 
+   // Funktion til håndtering af signup-processen
   const handleSignUp = () => {
     createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
+        // Viser meddelsesen 'success' hvis signup lykkedes 
         Alert.alert("Success", "User created successfully!");
         navigation.navigate('LoginScreen');
       })
       .catch((error) => {
+        // Viser fejlmeddelelsen 'error hvis signup fejler
         Alert.alert("Error", error.message);
       });
   };
 
+  // Returnerer viewet for signup-skærmen
   return (
     <KeyboardAvoidingView 
       style={globalStyles.container}
@@ -32,6 +46,7 @@ const SignUpScreen = ({ navigation }) => {
       <ScrollView>
         <View style={globalStyles.innerContainer}>
           <AppLogo source={require('../../assets/logos/appLogo.png')} />
+          {/* Input-felter for email og adgangskode */}
           <CustomTextInput
             placeholder="Email"
             value={email}
@@ -43,6 +58,7 @@ const SignUpScreen = ({ navigation }) => {
             onChangeText={setPassword}
             secureTextEntry
           />
+          {/* Knap til at udføre signup-handling */}
           <PrimaryButton title="Opret din bruger!" onPress={handleSignUp} />
         </View>
       </ScrollView>
@@ -50,5 +66,5 @@ const SignUpScreen = ({ navigation }) => {
   );
 };
 
+// Eksporterer SignUpScreen-komponenten som standard
 export default SignUpScreen;
-
